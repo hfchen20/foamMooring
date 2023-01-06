7bd8a62d2c8db245ed726218f50bd043fc4490d2
Haifei Chen <7517888-hfchen20@users.noreply.gitlab.com>
2023-01-06 15:04:19 -0700

    Remove md build

--- modules/MoorDyn.build/source/moordyn/MoorDynConfig.cmake
+++
@@ -1,5 +0,0 @@
-include(CMakeFindDependencyMacro)
-find_dependency(Eigen3 3.3)
-
-include("${CMAKE_CURRENT_LIST_DIR}/MoorDynTargets.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/MoorDynConfigVersion.cmake")
